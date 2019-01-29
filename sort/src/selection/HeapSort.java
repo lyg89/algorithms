4df85aa3086b69825b7e938661c6a2f116b4b745
Forrest Li <42837603+lyg89@users.noreply.github.com>
2019-01-30 00:45:33 +0800

    Create HeapSort.java

---
+++ sort/src/selection/HeapSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+
+public class HeapSort {
+    
+    public static void main(String[] args){
+
+        int[] arr = new int[]{5, 9, 7, 4, 22, 2, 65, 1, 45};
+        System.out.println("before select sort, the array is: ");
+        System.out.println(Arrays.toString(arr));
+
+        // 建最大堆
+        Heap heap = new Heap();
+        heap.buildMaxHeap(arr);
+
+        // 选取堆顶最大元素，放置到数组对应位置后，调整最大堆
+        for (int i = arr.length - 1; i > 0; i--) {
+            Utils.switchVal(arr, i, 0);
+            heap.setHeapSize(heap.getHeapSize() - 1);
+            heap.maxHeapify(arr, 0);
+        }
+        
+
+        System.out.println("\nend heap sort, the array is: ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
