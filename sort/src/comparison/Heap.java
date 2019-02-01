37140616e7f57e209af2c8ba209498ebb1dc0659
liyaguang <liyaguang@guolele.com>
2019-02-01 22:57:00 +0800

    improve code format

--- sort/src/comparison/Heap.java
+++ sort/src/comparison/Heap.java
@@ -2,37 +2,46 @@ package comparison;
 
 /**
  * max heap data structure
+ *
  * @author liyaguang
  */
 public class Heap {
     private int heapSize;
-    public int getHeapSize(){
+
+    public int getHeapSize() {
         return heapSize;
     }
-    public void setHeapSize(int size){
+
+    public void setHeapSize(int size) {
         this.heapSize = size;
     }
 
     public int getParentIdx(int i) {
-        return i/2;
+        return i / 2;
     }
 
     public int getLeftIdx(int i) {
-        return 2*i;
+        return 2 * i;
     }
 
     public int getRightIdx(int i) {
-        return 2*i + 1;
+        return 2 * i + 1;
     }
 
+    /**
+     * 调整最大堆，使其符合数据结构要求
+     *
+     * @param arr 源数组
+     * @param i   待调整元素在数组中的元素下标
+     */
     public void maxHeapify(int[] arr, int i) {
         int left = getLeftIdx(i);
         int right = getRightIdx(i);
         int largest = i;
-        if(left<heapSize && arr[left] > arr[largest]){
+        if (left < heapSize && arr[left] > arr[largest]) {
             largest = left;
         }
-        if(right<heapSize && arr[right] > arr[largest]) {
+        if (right < heapSize && arr[right] > arr[largest]) {
             largest = right;
         }
         if (largest != i) {
@@ -41,9 +50,14 @@ public class Heap {
         }
     }
 
+    /**
+     * 构建最大堆
+     *
+     * @param arr 源数组
+     */
     public void buildMaxHeap(int[] arr) {
         this.setHeapSize(arr.length);
-        for (int i = (arr.length/2); i >= 0; i--) {
+        for (int i = (arr.length / 2); i >= 0; i--) {
             maxHeapify(arr, i);
         }
     }
--- sort/src/comparison/SelectSort.java
+++ sort/src/comparison/SelectSort.java
@@ -8,23 +8,27 @@ import java.util.Arrays;
  * @create: 2018-12-03 13:26
  **/
 public class SelectSort {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         int[] arrOriginal = new int[]{5, 9, 7, 4, 22, 2, 65, 1, 45};
         System.out.println("before select sort, the array is: ");
         System.out.println(Arrays.toString(arrOriginal));
 
+        // 最小元素下标存放临时变量
         int smallestIdx;
+        // 每次待确定值的索引位置
         for (int i = 0; i < arrOriginal.length - 1; i++) {
             smallestIdx = i;
 
-            for (int j = i+1; j < arrOriginal.length; j++) {
+            // 遍历待排序序列，选出其中最小元素
+            for (int j = i + 1; j < arrOriginal.length; j++) {
                 if (arrOriginal[j] < arrOriginal[smallestIdx]) {
                     smallestIdx = j;
                 }
             }
-            
+
+            // 将最小值移动到它对应的索引位置
             Utils.switchVal(arrOriginal, i, smallestIdx);
         }
         System.out.println("\nend select sort, the array is: ");
