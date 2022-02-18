9179e07e0da19ab2bf8549e8ee0e61e2f290e6fd
lyg89 <liyaguang20111105@gmail.com>
2022-02-18 14:07:55 +0800

    greedy problem practise

---
+++ basic/src/p5greedy/Code04_LessMoneySplitGold.java
@@ -0,0 +1,65 @@
+package p5greedy;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author liyaguang11
+ * @date 2022/2/18
+ */
+public class Code04_LessMoneySplitGold {
+
+    // 纯暴力！
+    public static int lessMoney1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        return process(arr, 0);
+    }
+
+    // 等待合并的数都在arr里，pre之前的合并行为产生了多少总代价
+    // arr中只剩一个数字的时候，停止合并，返回最小的总代价
+    public static int process(int[] arr, int pre) {
+
+        return 0;
+    }
+
+
+    public static int lessMoney2(int[] arr) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i : arr) {
+            priorityQueue.add(i);
+        }
+
+        int sum = 0;
+        int cur;
+        while (priorityQueue.size() > 1) {
+            cur = priorityQueue.poll() + priorityQueue.poll();
+            sum += cur;
+            priorityQueue.add(cur);
+        }
+        return sum;
+    }
+
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxSize = 6;
+        int maxValue = 1000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            if (lessMoney1(arr) != lessMoney2(arr)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+}
---
+++ basic/src/p5greedy/Code05_IPO.java
@@ -0,0 +1,14 @@
+package p5greedy;
+
+/**
+ *
+ * https://leetcode-cn.com/problems/ipo/
+ * @author liyaguang11
+ * @date 2022/2/18
+ */
+public class Code05_IPO {
+
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        return 0;
+    }
+}
