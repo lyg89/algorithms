26bd7c2d8040feede482c53bba89d6ef6ffdb689
lyg89 <liyaguang20111105@gmail.com>
2022-03-01 12:53:52 +0800

    recursive hanoi

---
+++ basic/src/p8recursive/Code01_Hanoi.java
@@ -0,0 +1,33 @@
+package p8recursive;
+
+/**
+ * @author liyaguang11
+ * @date 2022/2/25
+ */
+public class Code01_Hanoi {
+
+    public static void hanoi2(int n) {
+        if (n > 0) {
+            process(n, "left", "right", "mid");
+        }
+    }
+
+    private static void process(int n, String from, String to, String other) {
+        if (n == 1) {
+            System.out.println("mov 1 from " + from + " to " + to);
+        } else {
+            process(n - 1, from, other, to);
+            System.out.println("mov " + n + " from " + from + " to " + to);
+            process(n - 1, other, to, from);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        //hanoi1(n);
+        System.out.println("============");
+        hanoi2(n);
+//		System.out.println("============");
+//		hanoi3(n);
+    }
+}
